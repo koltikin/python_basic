dc45a2da94c1ae4d69fcd3e08257e47455abfabd
koltikin <koltikin210@gmail.com>
2023-03-05 08:52:34 +0300

    day_01

---
+++ day_01/print.py
@@ -0,0 +1,18 @@
+# print("Hello World!", "my name is ziya", sep="||")
+# print("First line\nSecond line")
+
+# \n, \t, \\, \"
+
+# print("hello\nabdullah")
+# print("hello\tabdullah")
+# print("hello\\abdullah")
+# print("hello 'abdullah'")
+
+name = "abdullah"
+
+weight = 68.567
+# %s, %d %f, %c
+# print("my name is %s my age is %d" % (name, age))
+# print("my name is {0} my age is {1}".format(name, age))
+print(f"my name is {name} my weight {weight:010.1f} kG")
+# print(f"my name is {name} my age is {age}")
